3b4558b4d3f8e4618175f9739abe4b8b332a7496
belal <belalhezzat@gmail.com>
2022-01-26 19:47:16 +0200

    Updated configuration

---
+++ config/schedule.rb
@@ -0,0 +1,28 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+set :output, "./output.log"
+ENV.each { |k, v| env(k, v) }
+every 50.minutes do
+    runner "ApplicationsController.update_chats_count"
+end
+every 55.minutes do
+    runner "ChatsController.update_messages_count"
+end
